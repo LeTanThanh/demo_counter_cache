b872e4edb336085bb08007e34e50b9e72a76718e
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-08-25 12:42:48 +0700

    add_projects#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  # Has many association
   root "categories#index"
+
+  # Many to many association
   resources :users, only: :index
   resources :groups, only: :index
+
+  # Multi-level counter-cache
+  resources :projects, only: :index
 end
